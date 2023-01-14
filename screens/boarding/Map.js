9d4e443ea7a1e68882f9a47408b263e1c7a1091d
2anGel1 <angeemmanuelassamoi@gmail.com>
2023-01-14 05:28:41 +0000

    big up
    big up

--- screens/boarding/Map.js
+++ screens/boarding/Map.js
@@ -1,51 +1,123 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { Text, View, ScrollView, TextInput, Pressable } from 'react-native'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
-import Icon from 'react-native-vector-icons/AntDesign'
+import { Text, View, ActivityIndicator } from 'react-native'
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
 import IIcon from '../../elements/Icon.js'
 
 import colors from '../../style/colors'
 import styles from '../../style/styles'
+import customRetroMap from '../../style/customRetroMap.json'
 import variables from '../../style/variables'
 
+import * as Location from 'expo-location';
 
 
-const Map = ({navigation}) => {
+const Map = ({ navigation }) => {
 
 
-    return (
-        <View style={{ flex: 1 }}>
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
 
-            <MapView
-                style={{ flex: 1 }}
-                // provider={PROVIDER_GOOGLE}
-                initialRegion={{
-                    latitude: 5.322662032992469,
-                    longitude: -4.019345507466149,
-                    latitudeDelta: 0.01,
-                    longitudeDelta: 0.01
-                }}
-            />
+    const markers = [
+        {
+            key: 0,
+            title: 'Parking 1',
+            coordinates: {
+                latitude: "5.388906",
+                longitude: "-3.965171"
+            }
+        },
 
-            {/* <Icon
-                name="bars"
-                size={35}
-                color={colors.black}
-                style={{ position: 'absolute', top: 40, left: 20}}
-                onPress={() => {
-                    navigation.openDrawer()
-                }}
-                
-            /> */}
+        {
+            key: 1,
+            title: 'Parking 2',
+            coordinates: {
+                latitude: "5.387651",
+                longitude: "-3.962596"
+            }
+        }
+    ]
 
-            <IIcon action={() => {navigation.openDrawer()}}/>
+    useEffect(() => {
+        (async () => {
 
-            <StatusBar
-                backgroundColor={colors.white}
-            />
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    let load = true;
+    let longitude = "";
+    let latitude = "";
+
+    if(location){
+        load = false
+    }
+
+
+    if (errorMsg) {
+
+        <View style={styles.container}>
+            <Text>Permission denied</Text>
         </View>
-    )
+
+    } else if (!load) {
+        longitude = location.coords.longitude
+        latitude = location.coords.latitude
+
+        return (
+
+            <View style={{ flex: 1 }}>
+
+                <MapView
+                    style={{ flex: 1 }}
+                    provider={PROVIDER_GOOGLE}
+                    region={{
+                        latitude: latitude,
+                        longitude: longitude,
+                        latitudeDelta: 0.01,
+                        longitudeDelta: 0.01
+                    }}
+                    showsUserLocation={true}
+                    followsUserLocation={true}
+                    // customMapStyle={customRetroMap}
+                >
+
+                    {
+                        markers.map(marker => (
+                            <Marker key={marker.key} coordinate={marker.coordinates} title={marker.title}/>
+                        ))
+                    }
+
+                </MapView>
+
+                {/* <IIcon action={() => { navigation.openDrawer() }} /> */}
+
+                <StatusBar
+                    backgroundColor={colors.white}
+                />
+            </View>
+        )
+
+    } else if (load) {
+
+        return (
+            <View style={styles.container}>
+                <Text>Nous déterminons votre position...</Text>
+                <ActivityIndicator />
+            </View>
+        )
+
+    }
+
+
+
 }
 
 export default Map
